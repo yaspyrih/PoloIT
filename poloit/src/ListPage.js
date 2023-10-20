ed987676bd19f8c8c991664da8a5524ce55074d9
yaspyrih <yaspyrih@gmail.com>
2023-10-20 20:07:59 -0300

    Otra paleta CSS

--- poloit/src/ListPage.js
+++ poloit/src/ListPage.js
@@ -3,7 +3,7 @@ import Post from './Post';
 
 const ListPage = ({searchResults}) => {
     const results = searchResults.map(post => <Post key={post.id} post={post}/>)
-    const content = results?.length ? results : <article><p>La empresa no pertenece al Polo IT.</p></article>
+    const content = results?.length ? results : <article><h3>La empresa no pertenece al Polo IT.</h3></article>
   return (
     <main>{content}</main>
   )
