c5841892b1940eaa9378c5633a6c10c175793977
yaspyrih <yaspyrih@gmail.com>
2023-10-26 18:52:08 -0300

    Company category filter in sidebar

--- src/Companies/Companies.js
+++ src/Companies/Companies.js
@@ -4,11 +4,11 @@ function Companies() {
   return <>
   <section className='card-container'>
     <section className='card'>
-      <img src={img} alt='Logo de la empresa del Polo IT' className='card-img'></img>
-      <h2 className='company'>{company}</h2>
-      <p className='description'>{description}</p>
-      <h3 className='service'>{service}</h3>
-      <h4 className='category'>{category}</h4>
+      <img src="https://accionpoint.com/wp-content/uploads/2020/05/logo-ap@2x.png" alt='Logo de la empresa del Polo IT' className='card-img'></img>
+      <h2 className='company'>Acción point</h2>
+      <p className='description'>"Creamos y desarrollamos soluciones desde las necesidades de nuestros clientes, ayudando a descubrir nuevos caminos en la era de la transformación digital."</p>
+      <h3 className='service'>"Low-code platform, servicios financieros, innovación digital, desarrollos ágiles."</h3>
+      <h4 className='category'>"Software"</h4>
     </section>
   </section>
 
--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -2,23 +2,22 @@ import './Category.css';
 
 function Category() {
   return (
-    <div>
-      <h2 className='sidebar-title'>Category</h2>
-    </div>
-    <div>
-      <label className='sidebar-label-container'>
-        <input type="radio" name="test"/>
-        <span className='checkmark'></span>Todas
-      </label>
-      <label className='sidebar-label-container'>
-        <input type="radio" name="test"/>
-        <span className='checkmark'></span>Consultoría
-      </label>
-      <label className='sidebar-label-container'>
-        <input type="radio" name="test"/>
-        <span className='checkmark'></span>Informática
-      </label>
-    </div>
-  )
+    <><div>
+      <h2 className='sidebar-title'>Categoría</h2>
+    </div><div>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+          <span className='checkmark'></span>Todas
+        </label>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+          <span className='checkmark'></span>Consultoría
+        </label>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+          <span className='checkmark'></span>Informática
+        </label>
+      </div></>
+  );
 }
 export default Category
