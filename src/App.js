455980e2876e06a7318cfc68ec9c4266361a43dd
yaspyrih <yaspyrih@gmail.com>
2023-10-30 11:31:41 -0300

    Refactoring, calling data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,73 @@
+import { useState } from "react";
 import Navigation from "./Navigation/Nav";
 import Companies from "./Companies/Companies";
 import Sidebar from "./Sidebar/Sidebar";
 
+// ---------Database---------
+
+import companies from './db/data';
+import Card from "./components/Card";
+
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState("");
+
+// ---------Input filter-----------
+
+  
+
+  const handleInputChange = event => {
+    setQuery(event.target.value)
+  }
+
+  const filteredItems = companies.filter(company => company.company.toLocaleLowerCase().indexOf(query.toLocaleLowerCase()!==-1)
+  );
+
+// ---------Radio filter-----------
+
+const handleChange = event => {
+  setSelectedCategory(event.target.value)
+}
+
+// ---------Buttons filter-----------
+/* const handleClick = event =>{
+  setSelectedCategory(event.target.value)
+} */
+
+function filteredData(companies, selected, query) {
+  let filteredCompanies = companies
+
+//Filtering input items
+  if (query){
+    filteredCompanies = filteredItems
+  }
+
+  //Selected filter
+
+  if(selected) {
+    filteredCompanies = filteredCompanies.filter(
+      ({category, company, service})=>
+      category === selected||
+      company===selected || 
+      service===selected
+      );
+  }
+
+  return filteredCompanies.map(({img, company, description, service, category, web, linkedin, email})=>(
+    <Card 
+    key={Math.random()}
+    img={img}
+    company={company}
+    description={description}
+    service={service}
+    category={category}
+    web={web}
+    linkedin={linkedin}
+    email={email}
+    />
+  ))
+}
+
   return (
   <>
   <Sidebar/>
--- src/Sidebar/Service/Service.js
+++ src/Sidebar/Service/Service.js
@@ -1,45 +1,15 @@
+import Input from '../../components/Input';
 import './Service.css';
 
 function Service() {
   return (
     <div className='ml'>
       <h2 className='sidebar-title price-title'>Servicios</h2>
-      <label className='sidebar-label-container'>
-          <input type="radio" name="test2" />
-          <span className='checkmark'></span>Todos
-        </label>
-        <label className='sidebar-label-container'>
-          <input type="radio" name="test2" />
-          <span className='checkmark'></span>Consultoría
-        </label>
-        <label className='sidebar-label-container'>
-          <input type="radio" name="test2" />
-          <span className='checkmark'></span>Seguridad
-        </label>
-        <label className='sidebar-label-container'>
-          <input type="radio" name="test2" />
-          <span className='checkmark'></span>Testing
-        </label>
-        <label className='sidebar-label-container'>
-          <input type="radio" name="test2" />
-          <span className='checkmark'></span>Cloud
-        </label>
-        <label className='sidebar-label-container'>
-          <input type="radio" name="test2" />
-          <span className='checkmark'></span>Big Data
-        </label>
-        <label className='sidebar-label-container'>
-          <input type="radio" name="test2" />
-          <span className='checkmark'></span>Soporte
-        </label>
-        <label className='sidebar-label-container'>
-          <input type="radio" name="test2" />
-          <span className='checkmark'></span>Internet of Things
-        </label>
-        <label className='sidebar-label-container'>
-          <input type="radio" name="test2" />
-          <span className='checkmark'></span>Desarrollo
-        </label>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
       </div>
       
   )
